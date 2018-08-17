ac5595bfbcd1df0df75600c97e9d13b721777b3e
Entry Dev <entrydev@nts-corp.com>
2018-08-17 05:52:01 +0000

    Entry Tool PUBLISH by v1.20180817.1

--- src/actions/index.js
+++
@@ -1,6 +0,0 @@
-export const visibleAction = (visible) => (dispatch) => {
-    dispatch({
-        type: 'VISIBLE',
-        data: visible,
-    });
-};
--- src/reducers/index.js
+++
@@ -1,7 +0,0 @@
-import { combineReducers } from 'redux';
-import commonReducer from './common';
-import popupReducer from './popup';
-export default combineReducers({
-    commonReducer,
-    popupReducer,
-});
