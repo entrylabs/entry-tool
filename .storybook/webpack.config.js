162a3796356eeb58a993b5e96bbe921d125a645a
jykim <kimorkim@gmail.com>
2018-10-23 16:16:59 +0900

    css module 적용

--- .storybook/webpack.config.js
+++ .storybook/webpack.config.js
@@ -1,4 +1,5 @@
 const path = require('path');
+const autoprefixer = require('autoprefixer');
 const eslintFormatter = require('react-dev-utils/eslintFormatter');
 const ExtractTextPlugin = require('extract-text-webpack-plugin');
 const paths = require('../config/paths');
@@ -12,21 +13,56 @@ const extractTextPluginOptions = shouldUseRelativeAssetPaths
 module.exports = {
     module: {
         rules: [
+            // {
+            //     test: /\.scss$/,
+            //     loaders: [
+            //         require.resolve('style-loader'),
+            //         {
+            //             loader: require.resolve('css-loader'),
+            //             options: {
+            //                 importLoaders: 1,
+            //                 modules: true,
+            //                 localIdentName: '[name]__[local]___[hash:base64:5]',
+            //             },
+            //         },
+            //         require.resolve('sass-loader')
+            //     ],
+            //     include: path.resolve(__dirname, '../'),
+            // },
             {
-                test: /\.scss$/,
-                loaders: [
+                test: /\.(css|scss)$/,
+                use: [
                     require.resolve('style-loader'),
                     {
                         loader: require.resolve('css-loader'),
                         options: {
-                            importLoaders: 1,
                             modules: true,
-                            localIdentName: '[name]__[local]___[hash:base64:5]',
+                            importLoaders: 1,
                         },
                     },
-                    require.resolve('sass-loader')
+                    {
+                        loader: require.resolve('postcss-loader'),
+                        options: {
+                            // Necessary for external CSS imports to work
+                            // https://github.com/facebookincubator/create-react-app/issues/2677
+                            ident: 'postcss',
+                            plugins: () => [
+                                require('postcss-flexbugs-fixes'),
+                                autoprefixer({
+                                    browsers: [
+                                        '>1%',
+                                        'last 4 versions',
+                                        'Firefox ESR',
+                                        'not ie < 9', // React doesn't support IE8 anyway
+                                    ],
+                                    flexbox: 'no-2009',
+                                    remove: false,
+                                }),
+                            ],
+                        },
+                    },
+                    require.resolve('sass-loader'),
                 ],
-                include: path.resolve(__dirname, '../'),
             },
             {
                 test: [/\.bmp$/, /\.gif$/, /\.jpe?g$/, /\.png$/],
