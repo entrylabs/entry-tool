16206891846cccfa520b7a02e83722c6beeb4ac6
jykim <kimorkim@gmail.com>
2018-10-30 19:37:06 +0900

    add component

---
+++ component.js
@@ -0,0 +1,16 @@
+'use strict';
+
+Object.defineProperty(exports, '__esModule', {
+    value: true,
+});
+exports.ColorPicker = undefined;
+
+var _color = require('./src/components/picker/color');
+
+var _color2 = _interopRequireDefault(_color);
+
+function _interopRequireDefault(obj) {
+    return obj && obj.__esModule ? obj : { default: obj };
+}
+
+var ColorPicker = (exports.ColorPicker = _color2.default);
--- src/components/picker/color.jsx
+++ src/components/picker/color.jsx
@@ -5,6 +5,7 @@ import chroma from 'chroma-js';
 import { Object } from 'core-js';
 import { ColorPickAction } from '../../actions';
 import slider_handle from '../../styles/slider_handle.svg';
+import Styles from '../../assets/scss/popup.scss';
 
 const Slider = styled.div`
     background: white;
@@ -93,7 +94,8 @@ function getColorByHex(hex) {
 }
 
 function scaleRatioX(value) {
-    return Math.round(value * 1.8);
+    console.log(value);
+    return Math.round(value * 1.77);
 }
 
 class ColorPicker extends Component {
@@ -200,9 +202,8 @@ class ColorPicker extends Component {
         );
     }
 
-    handleChangeHsv(type, target) {
+    handleChangeHsv(type, value) {
         const { ColorPickAction } = this.props;
-        let { value = 0 } = target;
         value = parseInt(value, 10);
         this.setState((state) => {
             const hsv = Object.assign({}, state, { [type]: value });
@@ -223,91 +224,220 @@ class ColorPicker extends Component {
             return nextState;
         });
     }
-
+    move = (e) => {
+        if (this.canMoveCapture) {
+            const { clientX } = e;
+            let result = (clientX - this.sliderStartX) / 1.8 + this.sliderValue;
+            result = Math.round(Math.max(Math.min(result, 100), 0));
+            this.handleChangeHsv(this.sliderType, result);
+        }
+    };
+    up = () => {
+        console.log(this);
+        this.canMoveCapture = false;
+        this.setState(() => {
+            return {
+                isActiveSlider: null,
+            };
+        });
+    };
     componentDidMount() {
-        document.addEventListener('mousemove', ({ target }) => {
-            if (target.canMove) {
-            }
-        });
-        document.addEventListener('mouseup', ({ target }) => {
-            if (this.refs.indexOf(target) > -1) {
-                delete target.canMove;
-            }
-        });
+        document.addEventListener('mousemove', this.move);
+        document.addEventListener('mouseup', this.up);
     }
 
     componentWillUnmount() {
-        document.removeEventListener();
+        document.removeEventListener('mousemove', this.move);
+        document.removeEventListener('mouseup', this.up);
+    }
+
+    handleSliderMouseDown(e, type) {
+        const { nativeEvent = {} } = e;
+        const { clientX } = nativeEvent;
+        this.canMoveCapture = true;
+        this.sliderStartX = clientX;
+        this.sliderType = type;
+        this.sliderValue = this.state[type];
+        this.setState(() => {
+            return {
+                isActiveSlider: type,
+            };
+        });
     }
 
     render() {
-        const { hue, saturation, brightness } = this.state;
+        const { hue, saturation, brightness, red, green, blue, isActiveSlider } = this.state;
+        console.log(red, green, blue);
         return (
-            <div>
-                <Hue>
-                    <InputNumber
-                        value={hue}
-                        type="number"
-                        min="0"
-                        max="100"
-                        onChange={({ target }) => {
-                            this.handleChangeHsv('hue', target);
-                        }}
-                    />
-                    <SliderHandle
-                        innerRef={(node) => (this.hueSlider = node)}
-                        x={scaleRatioX(hue)}
-                        onMouseDown={(e) => {
-                            e.target.canMove = true;
-                            e.target.startPageX = e.pageX - hue * 1.8;
-                        }}
-                        onMouseMove={({ pageX, target }) => {
-                            if (target.canMove) {
-                                target.value = (pageX - target.startPageX) / 1.8;
-                                console.log(
-                                    pageX,
-                                    target.startPageX,
-                                    target.value,
-                                    pageX - target.startPageX
-                                );
-                                this.handleChangeHsv('hue', target);
-                            }
-                        }}
-                        onMouseUp={({ target }) => {
-                            target.canMove = false;
-                            console.log('onMouseUp', target.canMove);
-                        }}
-                    />
-                    <SliderBackground style={this.hueGradient()} />
-                </Hue>
-                <Saturation>
-                    <InputNumber
-                        value={saturation}
-                        type="number"
-                        min="0"
-                        max="100"
-                        onChange={({ target }) => {
-                            this.handleChangeHsv('saturation', target);
-                        }}
-                    />
-                    <SliderHandle x={scaleRatioX(saturation)} />
-                    <SliderBackground style={this.saturationGradient()} />
-                </Saturation>
-                <Value>
-                    <InputNumber
-                        value={brightness}
-                        type="number"
-                        min="0"
-                        max="100"
-                        onChange={({ target }) => {
-                            this.handleChangeHsv('brightness', target);
-                        }}
-                    />
-                    <SliderHandle x={scaleRatioX(brightness)} />
-                    <SliderBackground style={this.brightnessGradient()} />
-                </Value>
-                <Result style={this.resultBackground()} />
-                {this.makeRGBInput()}
+            <div className={Styles.popup_wrap}>
+                {/* 컬러피커 툴팁 */}
+                <div className={`${Styles.tooltip_box} ${Styles.color_picker} ${Styles.up}`}>
+                    <div className={`${Styles.tooltip_inner}`}>
+                        <div className={`${Styles.color_box}`}>
+                            <span
+                                className={`${Styles.color} ${Styles.imico_pop_circle_check}`}
+                                style={this.resultBackground()}
+                            >
+                                &nbsp;
+                            </span>
+                            <ul className={`${Styles.color_list}`}>
+                                <li className={`${Styles.item}`}>
+                                    <label htmlFor="red">빨강(R)</label>
+                                    <input
+                                        value={red}
+                                        type="number"
+                                        min="0"
+                                        max="255"
+                                        onChange={({ target }) => {
+                                            this.handleChangeRGB('red', target);
+                                        }}
+                                        id="red"
+                                        name="red"
+                                    />
+                                    <div className={`${Styles.graph}`} />
+                                </li>
+                                <li className={`${Styles.item}`}>
+                                    <label htmlFor="green">녹색(G)</label>
+                                    <input
+                                        value={green}
+                                        type="number"
+                                        min="0"
+                                        max="255"
+                                        onChange={({ target }) => {
+                                            this.handleChangeRGB('green', target);
+                                        }}
+                                        id="green"
+                                        name="green"
+                                    />
+                                </li>
+                                <li className={`${Styles.item}`}>
+                                    <label htmlFor="blue">파랑(B)</label>
+                                    <input
+                                        value={blue}
+                                        type="number"
+                                        min="0"
+                                        max="255"
+                                        onChange={({ target }) => {
+                                            this.handleChangeRGB('blue', target);
+                                        }}
+                                        id="blue"
+                                        name="blue"
+                                    />
+                                </li>
+                            </ul>
+                        </div>
+                        <div className={`${Styles.color_graph}`}>
+                            <ul className={`${Styles.graph_list}`}>
+                                <li className={`${Styles.item}`}>
+                                    <label htmlFor="hue">색상</label>
+                                    <input
+                                        value={hue}
+                                        type="number"
+                                        min="0"
+                                        max="100"
+                                        onChange={({ target }) => {
+                                            const { value = 0 } = target;
+                                            this.handleChangeHsv('hue', value);
+                                        }}
+                                        id="hue"
+                                        name="hue"
+                                    />
+                                    <div className={`${Styles.graph_box}`}>
+                                        {/* 그래프 slider가 선택되면 on 클래스 추가 */}
+                                        {/* slider 이동할 수 있도록  left값 조절 해주세요 */}
+                                        <span
+                                            className={`${Styles.slider} ${
+                                                Styles.btn_pop_color_slide
+                                            } ${isActiveSlider === 'hue' ? Styles.on : ''}`}
+                                            onMouseDown={(e) => {
+                                                this.handleSliderMouseDown(e, 'hue');
+                                            }}
+                                            style={{ left: scaleRatioX(hue) + 'px' }}
+                                        >
+                                            &nbsp;
+                                        </span>
+                                        <div
+                                            className={`${Styles.bar}`}
+                                            style={this.hueGradient()}
+                                        />
+                                    </div>
+                                </li>
+                                <li className={`${Styles.item}`}>
+                                    <label htmlFor="saturation">채도</label>
+                                    <input
+                                        value={saturation}
+                                        type="number"
+                                        min="0"
+                                        max="100"
+                                        onChange={({ target }) => {
+                                            const { value = 0 } = target;
+                                            this.handleChangeHsv('saturation', value);
+                                        }}
+                                        id="saturation"
+                                        name="saturation"
+                                    />
+                                    <div className={`${Styles.graph_box}`}>
+                                        {/* 그래프 slider가 선택되면 on 클래스 추가 */}
+                                        {/* slider 이동할 수 있도록  left값 조절 해주세요 */}
+                                        <span
+                                            className={`${Styles.slider} ${
+                                                Styles.btn_pop_color_slide
+                                            } ${isActiveSlider === 'saturation' ? Styles.on : ''}`}
+                                            onMouseDown={(e) => {
+                                                this.handleSliderMouseDown(e, 'saturation');
+                                            }}
+                                            style={{ left: scaleRatioX(saturation) + 'px' }}
+                                        >
+                                            &nbsp;
+                                        </span>
+                                        <div
+                                            className={`${Styles.bar}`}
+                                            style={this.saturationGradient()}
+                                        />
+                                    </div>
+                                </li>
+                                <li className={`${Styles.item}`}>
+                                    <label htmlFor="saturation">명도</label>
+                                    <input
+                                        value={brightness}
+                                        type="number"
+                                        min="0"
+                                        max="100"
+                                        onChange={({ target }) => {
+                                            const { value = 0 } = target;
+                                            this.handleChangeHsv('brightness', value);
+                                        }}
+                                        id="lightness"
+                                        name="lightness"
+                                    />
+                                    <div className={`${Styles.graph_box}`}>
+                                        {/* 그래프 slider가 선택되면 on 클래스 추가 */}
+                                        {/* slider 이동할 수 있도록  left값 조절 해주세요 */}
+                                        <span
+                                            className={`${Styles.slider} ${
+                                                Styles.btn_pop_color_slide
+                                            } ${isActiveSlider === 'brightness' ? Styles.on : ''}`}
+                                            onMouseDown={(e) => {
+                                                this.handleSliderMouseDown(e, 'brightness');
+                                            }}
+                                            style={{ left: scaleRatioX(brightness) + 'px' }}
+                                        >
+                                            &nbsp;
+                                        </span>
+                                        <div
+                                            className={`${Styles.bar}`}
+                                            style={this.brightnessGradient()}
+                                        />
+                                    </div>
+                                </li>
+                            </ul>
+                        </div>
+                    </div>
+                    {/* left 값 조절로 화살표 위치 잡을 수 있습니다. */}
+                    <span className={`${Styles.arr} ${Styles.free}`} style={{ left: 101 + 'px' }}>
+                        <i />
+                    </span>
+                </div>
             </div>
         );
     }
