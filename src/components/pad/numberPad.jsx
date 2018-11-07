b9f6795efa861441ea7c9e0ab0acb67e6ac949ec
extracold1209 <extracold1209@gmail.com>
2018-11-07 18:32:15 +0900

    숫자패드 뼈대 복사

--- src/components/pad/numberPad.jsx
+++ src/components/pad/numberPad.jsx
@@ -1,10 +1,22 @@
 import React, { Component } from 'react';
 import { pure } from 'recompose';
+import Styles from '../../assets/scss/popup.scss';
 import OutsideClick from '../common/outsideClick';
+
+
+/* eslint-disable array-element-newline */
+const keyPadList = [
+    '7', '8', '9',
+    '4', '5', '6',
+    '1', '2', '3',
+    '-', '0', '.',
+];
+
+/* eslint-disable array-element-newline */
 
 class NumberPad extends Component {
     constructor(props) {
-        super();
+        super(props);
     };
 
     render() {
@@ -17,10 +29,29 @@ class NumberPad extends Component {
                         onOutsideClick();
                     }
                 }}
-                eventTypes={['click', 'touchstart']}
+                eventTypes={['mouseup', 'touchend']}
             >
-                <div>
-                    Hello NumberPad
+                <div
+                    className={`${Styles.tooltip_box} ${Styles.pad_only} ${Styles.up}`}
+                >
+                    <div className={Styles.tooltip_inner}>
+                        <div className={Styles.time_board}>
+                            {keyPadList.map((value) => (
+                                <a className={Styles.btn_cnt} key={value}>
+                                    {value}
+                                </a>
+                            ))}
+                            <a
+                                className={`${Styles.btn_cnt} ${Styles.btn_del} ${Styles.imico_pop_key_del}`}
+                            >
+                                <span className={Styles.blind}>지우기</span>
+                            </a>
+                        </div>
+                    </div>
+                    {/* left 값 조절로 화살표 위치 잡을 수 있습니다. */}
+                    <span className={Styles.arr}>
+                        <i/>
+                    </span>
                 </div>
             </OutsideClick>
         );
