8934bb341ce0b3248b310b49707a099bb96f891a
jykim <kimorkim@gmail.com>
2018-11-20 14:41:34 +0900

    폴리필 오류 수정

--- config/polyfills.js
+++ config/polyfills.js
@@ -26,7 +26,7 @@ window.matchMedia =
     function() {
         return {
             matches: false,
-            addListener() {},
-            removeListener() {},
+            addListener: function() {},
+            removeListener: function() {},
         };
     };
