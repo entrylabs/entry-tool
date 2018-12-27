5a12b1c65bf61e215ef039ed76e420450a891b6d
kimorkim <kimorkim@gmail.com>
2018-12-27 16:57:08 +0900

    disable eslint

--- src/utils/Common.js
+++ src/utils/Common.js
@@ -24,6 +24,7 @@ export const CommonUtils = {
         return falseClassName;
     },
     fixUrl(url) {
+        /* eslint-disable no-useless-escape */
         const expUrl = /^[((http(s?))\:\/\/?)|/]/i;
         return expUrl.test(url) ? url : `/${url}`;
     },
