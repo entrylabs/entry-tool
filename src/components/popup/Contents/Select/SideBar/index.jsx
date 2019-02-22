fe15c696998ad151f2a5cb26d009cac104b59828
nt11203 <lee.hungyu@nts-corp.com>
2019-02-22 21:16:05 +0900

    [#10038] 방어로직 추가.

--- src/components/popup/Contents/Select/SideBar/index.jsx
+++ src/components/popup/Contents/Select/SideBar/index.jsx
@@ -10,12 +10,19 @@ import { triggerEvent } from '@actions';
 import { setUIParam } from '@actions/popup';
 import { EMIT_TYPES } from '@constants';
 
+const availableTypes = [
+    'picture',
+    'sprite',
+    'paint',
+    'sound',
+];
+
 class Index extends Component {
     constructor(props) {
         super(props);
         const isEmpty = this.props.data.data.length === 0;
         this.props.setUIParam(this.options);
-        if (isEmpty) {
+        if (isEmpty && availableTypes.includes(this.props.type)) {
             this.props.triggerEvent(EMIT_TYPES.fetch, this.options, false);
         }
         this.drawItems = this.drawItems.bind(this);
