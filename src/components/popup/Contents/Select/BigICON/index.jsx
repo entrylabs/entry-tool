946f6f72fecb7a7b1dea4440a1455abb2a2758d0
nt11203 <lee.hungyu@nts-corp.com>
2019-03-11 12:27:57 +0900

    [#10030] 확장블록 불러오기 안내문구 추가.

--- src/components/popup/Contents/Select/BigICON/index.jsx
+++ src/components/popup/Contents/Select/BigICON/index.jsx
@@ -18,6 +18,10 @@ class Index extends Component {
                     <div className={Styles.section_cont}>
                         <h2 className={Styles.blind}>확장 블록 불러오기 리스트</h2>
                         <div className={Styles.cont_box}>
+                            <div className={Styles.desc}>
+                                <div className={Styles.imico_exclamation_mark}></div>
+                                <div className={Styles.content}>확장 블록은 인터넷이 연결되어 있어야 정상적으로 동작합니다.</div>
+                            </div>
                             <div className={Styles.extend_block}>
                                 <ul className={Styles.list}>{this.drawItems()}</ul>
                             </div>
