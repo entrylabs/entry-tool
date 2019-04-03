c25ce81405af9f58dc4e6cad634135cbf74a842d
nt11203 <lee.hungyu@nts-corp.com>
2019-04-03 12:31:35 +0900

    entry-tool index에  component 추가.

--- src/index.jsx
+++ src/index.jsx
@@ -1,3 +1,13 @@
+import Dropdown from './components/widget/dropdown';
+import ModalProgress from './components/widget/modalProgress';
+import ColorPicker from './components/picker/color';
+
+export const Component = {
+    Dropdown,
+    ModalProgress,
+    ColorPicker,
+};
+
 export { default as ColorPicker } from './components/picker/colorContainer';
 export { default as Sortable } from './components/widget/sortableContainer';
 export { default as Dropdown } from './components/widget/dropdownContainer';
@@ -6,7 +16,3 @@ export { default as Number } from './components/widget/numberContainer';
 export { default as ContextMenu } from './components/widget/contextMenuContainer';
 export { default as BackPack } from './components/widget/BackPackContainer';
 export { default as Popup } from './components/popup/popupContainer';
-
-export { default as ColorPickerComponent } from './components/picker/color';
-export { default as DropdownComponent } from './components/widget/dropdown';
-export { default as ModalProgressComponent } from './components/widget/modalProgress';
