7b02a594c23eb827b1ddbe3b66d6c9489d282133
kimorkim <kimorkim@gmail.com>
2019-05-17 15:47:21 +0900

    change P -> p

--- src/utils/Theme.js
+++ src/utils/Theme.js
@@ -1,5 +1,5 @@
 import EntryPopupStyle from '@assets/entry/scss/popup.scss';
-import EntryBackPackStyle from '@assets/entry/scss/widget/BackPack.scss';
+import EntryBackpackStyle from '@assets/entry/scss/widget/Backpack.scss';
 import EntryProgress from '@assets/entry/scss/progress.scss';
 import EntryLinePopupStyle from '@assets/entryline/scss/popup.scss';
 
@@ -9,12 +9,12 @@ class Theme {
         this.styles = {
             entry: {
                 popup: EntryPopupStyle,
-                backpack: EntryBackPackStyle,
+                backpack: EntryBackpackStyle,
                 progress: EntryProgress,
             },
             entryline: {
                 popup: EntryLinePopupStyle,
-                backpack: EntryBackPackStyle,
+                backpack: EntryBackpackStyle,
                 progress: EntryLinePopupStyle,
             },
         };
