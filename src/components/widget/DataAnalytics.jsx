3a1752f724526c334d0d348680366f769d1e7fbe
kimorkim <kimorkim@gmail.com>
2020-01-07 15:24:10 +0900

    add DataAnalytics

---
+++ src/components/widget/DataAnalytics.jsx
@@ -0,0 +1,13 @@
+import React, { Component } from 'react';
+
+class DataAnalytics extends Component {
+    constructor(props) {
+        super(props);
+        console.log(props);
+    }
+    render() {
+        return <div>a</div>;
+    }
+}
+
+export default DataAnalytics;
