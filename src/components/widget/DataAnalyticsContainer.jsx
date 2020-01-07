c12db09941d3dc2ea217960ef86fc0d51fd30429
kimorkim <kimorkim@gmail.com>
2020-01-07 15:24:10 +0900

    add DataAnalytics

---
+++ src/components/widget/DataAnalyticsContainer.jsx
@@ -0,0 +1,11 @@
+import React, { Component } from 'react';
+import DataAnalytics from './DataAnalytics';
+import withWrapper from '@hoc/withWrapper';
+
+class DataAnalyticsContainer extends Component {
+    render() {
+        return <DataAnalytics {...this.props} />;
+    }
+}
+
+export default withWrapper({ type: 'widget' })(DataAnalyticsContainer);
