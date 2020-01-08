53c5c14222a015b3675881a00c098cb0603bb1a1
김영호 <young-ho.kim@nhn.com>
2020-01-08 12:36:57 +0900

    테이블 추가하기 - 데이터 선택 / 데이터 업로드 완료

--- src/stories/index.js
+++ src/stories/index.js
@@ -31,12 +31,15 @@ import configureStore from '../store';
 import Chart from '../components/widget/Chart';
 import Table from '../components/widget/Table';
 
+import DataSelect from '../components/ai_layout/DataSelect';
+import DataUpload from '../components/ai_layout/DataUpload';
+
 const store = configureStore();
 httpService.setupInterceptors('http://localhost:4000');
 export default function Provider({ story }) {
     return <ReduxProvider store={store}>{story}</ReduxProvider>;
 }
-Theme.type = 'entryline';
+//Theme.type = 'entryline';
 storiesOf('Popup', module)
     .addDecorator((story) => <Provider story={story()} />)
     .add('전체', () => <Sample />)
@@ -246,3 +249,9 @@ wigetStories
     })
     .add('차트', () => <Chart table={TABLE_SAMPLE} />)
     .add('테이블', () => <Table table={TABLE_SAMPLE} editor={'text'} />);
+
+storiesOf('AiLayout', module) 
+    .addDecorator((story) => <Provider story={story()}/>)
+    .add('테이블 추가하기 - 데이터 선택', () => <DataSelect />)
+    .add('테이블 추가하기 - 데이터 선택 딤드', () => <DataSelect Dimmed />)
+    .add('테이블 추가하기 - 데이터 업로드', () => <DataUpload />);
