6f634fe60c41fcea3c1cd0099ef774876ec7e954
김영호 <young-ho.kim@nts-corp.com>
2020-01-20 19:19:44 +0900

    그래프 범례 X축 / 통합 범례

--- src/components/ai_layout/DataDetail.jsx
+++ src/components/ai_layout/DataDetail.jsx
@@ -339,7 +339,93 @@ class DataDetail extends Component {
                                 </div>
                             </div>
                             <div className={Styles.cont_inner}>
-                                <div className={Styles.chart_box}>{/* 그래프를 넣어주세요 */}</div>
+                                <div className={Styles.chart_box}>
+                                    {/* 그래프를 넣어주세요 */}
+                                    <div className={Styles.x_legend}>
+                                        <a href="#">학교명</a>
+                                        {/* 학교명이 클릭되면 style="display:block" 추가 */}
+                                        <div className={`${Styles.tooltip_box} ${Styles.down}`}
+                                            style={{ display: 'block' }}
+                                        >
+                                            <div className={Styles.tooltip_inner}>
+                                                <ul className={Styles.select_list}>
+                                                    <li className={Styles.list_item}>
+                                                        <div className={Styles.list_link}>
+                                                            구분
+                                                        </div>
+                                                    </li>
+                                                    <li className={Styles.list_item}>
+                                                        <div className={Styles.list_link}>
+                                                            학교명
+                                                        </div>
+                                                    </li>
+                                                    <li className={Styles.list_item}>
+                                                        <div className={Styles.list_link}>
+                                                            주소
+                                                        </div>
+                                                    </li>
+                                                    <li className={Styles.list_item}>
+                                                        <div className={Styles.list_link}>
+                                                            교무실
+                                                        </div>
+                                                    </li>
+                                                </ul>
+                                            </div>
+                                            <span className={Styles.arr}>
+                                                <i />
+                                            </span>
+                                        </div>
+                                    </div>
+                                    <div className={Styles.legend_box}>
+                                        <a href="#" className={Styles.legend_link}>범례</a>
+                                        {/* 학교명이 클릭되면 style="display:block" 추가 */}
+                                        <div className={Styles.tooltip_box}
+                                            style={{ display: 'block' }}
+                                        >
+                                            <div className={Styles.tooltip_inner}>
+                                                <ul className={Styles.select_list}>
+                                                    <li className={Styles.list_item}>
+                                                        <div className={Styles.chk_box}>
+                                                            <input type="checkbox" id="chk1" name="chk1" />
+                                                            <label htmlFor="chk1">범례1</label>
+                                                        </div>
+                                                    </li>
+                                                    <li className={Styles.list_item}>
+                                                        <div className={Styles.chk_box}>
+                                                            <input type="checkbox" id="chk2" name="chk2" />
+                                                            <label htmlFor="chk2">범례2</label>
+                                                        </div>
+                                                    </li>
+                                                    <li className={Styles.list_item}>
+                                                        <div className={Styles.chk_box}>
+                                                            <input type="checkbox" id="chk3" name="chk3" />
+                                                            <label htmlFor="chk3">범례3</label>
+                                                        </div>
+                                                    </li>
+                                                </ul>
+                                            </div>
+                                            <span className={Styles.arr}>
+                                                <i />
+                                            </span>
+                                        </div>
+                                        <ul className={Styles.legend_list}>
+                                            <li>
+                                                {/* 그래프 색상 값을 넣어주세요. */}
+                                                <span 
+                                                    className={Styles.color}
+                                                    style={{ backgroundColor: '#4f80ff' }}
+                                                >&nbsp;</span>예시1
+                                            </li>
+                                            <li>
+                                                {/* 그래프 색상 값을 넣어주세요. */}
+                                                <span 
+                                                    className={Styles.color}
+                                                    style={{ backgroundColor: '#ffb500' }}
+                                                >&nbsp;</span>예시2
+                                            </li>
+                                        </ul>
+                                    </div>
+                                </div>
                             </div>
                         </div>
                     </section>
