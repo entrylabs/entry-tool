e3cb83a24fc2dfea489df5427a0ed83be948662a
USER <user@AL01461572.local>
2020-01-23 18:30:56 +0900

    AI - 차트 레이아웃 변경 / 차트 부분은 추후 커밋 예정

--- src/stories/index.js
+++ src/stories/index.js
@@ -259,8 +259,10 @@ storiesOf('AiLayout', module)
     .add('테이블 추가하기 - 데이터 선택 딤드', () => <DataSelect Dimmed />)
     .add('테이블 추가하기 - 데이터 업로드', () => <DataUpload />)
     .add('데이터 상세 - 요약', () => <DataDetail />)
+    .add('데이터 상세 - 데이터 없음', () => <DataDetail DetailState="DataOff" />)
     .add('데이터 상세 - 테이블', () => <DataDetail DetailState="Table" />)
     .add('데이터 상세 - 차트', () => <DataDetail DetailState="Chart" />)
+    .add('데이터 상세 - 차트 데이터 없음', () => <DataDetail DetailState="ChartDataOff" />)
     .add('차트', () => (
         <Chart table={[TABLE_SAMPLE2.fields, ...TABLE_SAMPLE2.origin]} type="line" />
     ))
