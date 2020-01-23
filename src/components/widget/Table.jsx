fc6234f0dfb60fef8bf3b790fc912eeafbea8819
kimorkim <kimorkim@gmail.com>
2020-01-23 18:50:31 +0900

    change table

--- src/components/widget/Table.jsx
+++ src/components/widget/Table.jsx
@@ -6,13 +6,13 @@ import { Prompt } from '@entrylabs/modal';
 import Theme from '@utils/Theme';
 import { getHeader, getData } from '@utils/Common';
 import ContextMenu from '../widget/contextMenu';
+import OutsideClick from '../common/outsideClick';
 
 import 'tui-grid/dist/tui-grid.css';
 import '@entrylabs/modal/dist/entry/entry-modal.css';
 
 const LEFT_CLICK = 1;
 const RIGHT_CLICK = 3;
-let removeColumnIndex = -1;
 
 TuiGrid.applyTheme('entry', {
     cell: {
@@ -50,6 +50,8 @@ const isColumnDblClick = ({ nativeEvent, columnName, targetType }) => {
 };
 
 const Table = (props) => {
+    const [table, setTable] = useState();
+    const gridRef = useRef();
     const [{ x, y, isVisible, contextMenu }, setContextMenuOption] = useState({
         x: 0,
         y: 0,
@@ -60,19 +62,12 @@ const Table = (props) => {
         showPrompt: false,
         promptText: '',
     });
-    const { table: tableProps = [] } = props;
-    const [table, setTable] = useState(tableProps);
-    const gridRef = useRef();
-
-    useEffect(() => {
-        setTable(tableProps);
-    }, [tableProps]);
-
     const {
+        table: tableProps = [],
         width,
         bodyHeight = 510,
         columnOptions = {
-            minWidth: 130,
+            minWidth: 150,
         },
         editable = 'text',
         rowHeight = 40,
@@ -80,36 +75,113 @@ const Table = (props) => {
         needRowHeader = true,
     } = props;
 
-    const handleNameChange = (name, index) => {
-        setTable((table) => {
-            table[0][index] = name;
-            return table;
+    useEffect(() => {
+        setTable(tableProps);
+    }, [tableProps]);
+
+    const handleNameChange = (index) => (name) => {
+        if (name) {
+            setTable((table) => {
+                table[0][index] = name;
+                return table;
+            });
+        }
+        setShowPrompt({
+            showPrompt: false,
         });
     };
 
     const handleClick = useCallback(
         (event) => {
-            const { nativeEvent } = event;
-            const { which, target } = nativeEvent;
-            const { cellIndex } = target;
-            const [fields] = table;
-            if (which === LEFT_CLICK && isColumnDblClick(event)) {
+            if (!table) {
+                return;
+            }
+
+            const { instance, columnName, nativeEvent } = event;
+            const { which } = nativeEvent;
+            if (!columnName) {
+                instance.finishEditing();
+            } else if (which === LEFT_CLICK && isColumnDblClick(event)) {
+                const colIndex = instance.getIndexOfColumn(columnName);
+                const [fields] = table;
                 setShowPrompt({
                     showPrompt: true,
-                    promptText: fields[cellIndex],
-                    promptFunction: (name) => {
-                        if (name) {
-                            handleNameChange(name, cellIndex);
-                        }
-                        setShowPrompt({
-                            showPrompt: false,
-                        });
-                    },
+                    promptText: fields[colIndex],
+                    promptFunction: handleNameChange(colIndex),
                 });
             }
         },
-        [table]
+        [table, setTable, setShowPrompt]
     );
+
+    const makeContextMenu = (event) => {
+        const { targetType, columnName, rowKey, instance } = event;
+        if (targetType === 'rowHeader') {
+            const rowIndex = instance.getIndexOfRow(rowKey) + 1;
+            return [
+                {
+                    text: '위에 행 추가하기',
+                    callback: () => {
+                        setTable((table) => {
+                            table.splice(rowIndex, 0, Array(table[0].length).fill(0));
+                            return table;
+                        });
+                    },
+                },
+                {
+                    text: '아래에 행 추가하기',
+                    callback: () => {
+                        setTable((table) => {
+                            table.splice(rowIndex + 1, 0, Array(table[0].length).fill(0));
+                            return table;
+                        });
+                    },
+                },
+                {
+                    text: '행 삭제하기',
+                    callback: () => {
+                        setTable((table) => {
+                            table.splice(rowIndex, 1);
+                            return table;
+                        });
+                    },
+                },
+            ];
+        } else if (targetType === 'columnHeader' && columnName !== '_number') {
+            const colIndex = instance.getIndexOfColumn(columnName);
+            return [
+                {
+                    text: '왼쪽에 속성 추가하기',
+                    callback: () => {
+                        setShowPrompt({
+                            showPrompt: true,
+                            promptFunction: handleAddColumn(colIndex),
+                        });
+                    },
+                },
+                {
+                    text: '오른쪽에 속성 추가하기',
+                    callback: () => {
+                        setShowPrompt({
+                            showPrompt: true,
+                            promptFunction: handleAddColumn(colIndex + 1),
+                        });
+                    },
+                },
+                {
+                    text: '속성 삭제하기',
+                    callback: () => {
+                        setTable((table) =>
+                            table.map((row) => {
+                                row.splice(colIndex, 1);
+                                return row;
+                            })
+                        );
+                    },
+                },
+            ];
+        }
+    };
 
     const handleMousedown = useCallback(
         (event) => {
@@ -118,111 +190,42 @@ const Table = (props) => {
             if (which !== RIGHT_CLICK) {
                 return;
             }
-
-            const { targetType, columnName, rowKey } = event;
-            let contextMenu = [];
-            if (targetType === 'columnHeader' && columnName !== '_number') {
-                contextMenu = makeContextMenu(table, setTable, 'column', columnName);
-            } else if (targetType === 'rowHeader') {
-                contextMenu = makeContextMenu(table, setTable, 'row', rowKey);
-            } else {
-                return;
+            const contextMenu = makeContextMenu(event);
+            if (contextMenu) {
+                setContextMenuOption(() => ({ x, y, contextMenu, isVisible: true }));
             }
-            setContextMenuOption(() => ({ x, y, contextMenu, isVisible: true }));
         },
-        [table]
+        [setTable]
     );
 
     const handleOutsideClick = () => {
-        setContextMenuOption((prev) => ({ ...prev, isVisible: false }));
+        setContextMenuOption({ isVisible: false });
     };
 
-    const handleAddColumn = (columnName) => {
-        if (removeColumnIndex === -1) {
-            return;
-        }
+    const handleAddColumn = (colIndex) => (columnName) => {
         setShowPrompt({
             showPrompt: false,
         });
-        // todo: columnName 중복 제거 로직 추가
-        setTable(
+        if (colIndex === -1 || !columnName) {
+            return;
+        }
+        setTable((table) =>
             table.map((row, index) => {
-                row.splice(removeColumnIndex, 0, index ? 0 : columnName);
+                row.splice(colIndex, 0, index ? 0 : columnName);
                 return row;
             })
         );
-        removeColumnIndex = -1;
     };
 
-    const handleEditingFinish = useCallback((event) => {
-        tableProps[event.rowKey + 1][table[0].findIndex((header) => event.columnName === header)] =
-            event.value;
-    });
-
-    const makeContextMenu = (targetTable, setTable, type, key) => {
-        const table = [...targetTable];
-        if (type === 'row') {
-            return [
-                {
-                    text: '위에 행 추가하기',
-                    callback: () => {
-                        table.splice(key + 1, 0, Array(table[0].length).fill(0));
-                        setTable(table);
-                    },
-                },
-                {
-                    text: '아래에 행 추가하기',
-                    callback: () => {
-                        setTable(table.splice(key + 2, 0, Array(table[0].length).fill(0)));
-                        setTable(table);
-                    },
-                },
-                {
-                    text: '행 삭제하기',
-                    callback: () => {
-                        setTable(table.splice(key + 1, 1));
-                        setTable(table);
-                    },
-                },
-            ];
-        } else if (type === 'column') {
-            return [
-                {
-                    text: '왼쪽에 속성 추가하기',
-                    callback: () => {
-                        removeColumnIndex = table[0].findIndex((header) => key === header);
-                        setShowPrompt({
-                            showPrompt: true,
-                            promptFunction: handleAddColumn,
-                        });
-                    },
-                },
-                {
-                    text: '오른쪽에 속성 추가하기',
-                    callback: () => {
-                        removeColumnIndex = table[0].findIndex((header) => key === header) + 1;
-                        setShowPrompt({
-                            showPrompt: true,
-                            promptFunction: handleAddColumn,
-                        });
-                    },
-                },
-                {
-                    text: '속성 삭제하기',
-                    callback: () => {
-                        const index = table[0].findIndex((header) => key === header);
-                        setTable(
-                            table.map((row) => {
-                                row.splice(index, 1);
-                                return row;
-                            })
-                        );
-                    },
-                },
-            ];
-        }
-        return [{ text: ' ' }, handleAddColumn];
-    };
+    const handleEditingFinish = useCallback(
+        (event) => {
+            const { instance, columnName, rowKey } = event;
+            const colIndex = instance.getIndexOfColumn(columnName);
+            const rowIndex = instance.getIndexOfRow(rowKey);
+            tableProps[rowIndex][colIndex] = event.value;
+        },
+        [tableProps]
+    );
 
     const data = getData(table);
     const columns = getHeader(table, editable);
@@ -230,21 +233,30 @@ const Table = (props) => {
 
     return (
         <div className={`${theme.Table} ${theme.tui_grid}`} onContextMenu={handleContextMenu}>
-            <Grid
-                ref={gridRef}
-                data={data}
-                columns={columns}
-                width={width}
-                rowHeight={rowHeight}
-                bodyHeight={bodyHeight}
-                columnOptions={columnOptions}
-                virtualScrolling={true}
-                usageStatistics={false}
-                rowHeaders={needRowHeader ? rowHeaders : {}}
-                onMousedown={handleMousedown}
-                onClick={handleClick}
-                onEditingFinish={handleEditingFinish}
-            />
+            <OutsideClick
+                onOutsideClick={() => {
+                    const { current = {} } = gridRef;
+                    if (current.getInstance) {
+                        current.getInstance().finishEditing();
+                    }
+                }}
+            >
+                <Grid
+                    ref={gridRef}
+                    data={data}
+                    columns={columns}
+                    width={width}
+                    rowHeight={rowHeight}
+                    bodyHeight={bodyHeight}
+                    columnOptions={columnOptions}
+                    virtualScrolling={true}
+                    usageStatistics={false}
+                    rowHeaders={needRowHeader ? rowHeaders : {}}
+                    onMousedown={handleMousedown}
+                    onClick={handleClick}
+                    onEditingFinish={handleEditingFinish}
+                />
+            </OutsideClick>
             {isVisible && (
                 <ContextMenu
                     onOutsideClick={handleOutsideClick}
