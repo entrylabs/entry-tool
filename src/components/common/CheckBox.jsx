74d0781e4a3def666e12a63f5504031d5f3563f7
박영덕 <prisml@nate.com>
2020-02-10 14:30:42 +0900

    [CheckBox] warning 수정
    - You provided a `checked` prop to a form field without an `onChange` handler.

--- src/components/common/CheckBox.jsx
+++ src/components/common/CheckBox.jsx
@@ -5,7 +5,7 @@ import Theme from '@utils/Theme';
 const CheckBox = (props) => {
     const { checked } = props;
     const theme = Theme.getStyle('popup');
-    return <input type="checkbox" className={`${theme.checkbox}`} checked={checked} />;
+    return <input type="checkbox" className={`${theme.checkbox}`} checked={checked} readOnly />;
 };
 
 export default CheckBox;
