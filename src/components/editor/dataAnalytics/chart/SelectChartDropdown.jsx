db9f51a2aca64df1afefbf2e7ff05edab158a4ac
DESKTOP-P667AO1\EDIT <youngduk.park@nhn.com>
2020-04-06 19:43:15 +0900

    [SelectChartDropdown] 마크업 수정

--- src/components/editor/dataAnalytics/chart/SelectChartDropdown.jsx
+++ src/components/editor/dataAnalytics/chart/SelectChartDropdown.jsx
@@ -10,44 +10,42 @@ const SelectChartDropdown = (props) => {
     };
 
     return (
-        <div className={Styles.tooltip_box} style={{ width: '242px', ...position }}>
-            <div className={Styles.tooltip_inner} style={{ padding: '16px' }}>
-                <ul className={Styles.list}>
-                    <li className={Styles.bar} onClick={onClick('bar')}>
-                        <a href="#" onClick={handleAClick}>
-                            {CommonUtils.getLang('DataAnalytics.bar')}
-                            <span className={Styles.blind}>
-                                {CommonUtils.getLang('DataAnalytics.graph')}
-                            </span>
-                        </a>
-                    </li>
-                    <li className={Styles.line} onClick={onClick('line')}>
-                        <a href="#" onClick={handleAClick}>
-                            {CommonUtils.getLang('DataAnalytics.line')}
-                            <span className={Styles.blind}>
-                                {CommonUtils.getLang('DataAnalytics.graph')}
-                            </span>
-                        </a>
-                    </li>
-                    <li className={Styles.pie} onClick={onClick('pie')}>
-                        <a href="#" onClick={handleAClick}>
-                            {CommonUtils.getLang('DataAnalytics.pie')}
-                            <span className={Styles.blind}>
-                                {CommonUtils.getLang('DataAnalytics.graph')}
-                            </span>
-                        </a>
-                    </li>
-                    <li className={Styles.scatter} onClick={onClick('scatter')}>
-                        <a href="#" onClick={handleAClick}>
-                            {CommonUtils.getLang('DataAnalytics.scatter')}
-                            <span className={Styles.blind}>
-                                {CommonUtils.getLang('DataAnalytics.graph')}
-                            </span>
-                        </a>
-                    </li>
-                </ul>
-            </div>
-            <span className={`${Styles.arr} ${Styles.free}`} style={{ left: '6px' }}>
+        <div className={Styles.vertical_tooltip} style={{ left: '56px', display: 'block' }}>
+            <ul className={Styles.graph_list}>
+                <li className={Styles.bar} onClick={onClick('bar')}>
+                    <a href="#" onClick={handleAClick} role="button">
+                        {CommonUtils.getLang('DataAnalytics.bar')}
+                        <span className={Styles.blind}>
+                            {CommonUtils.getLang('DataAnalytics.graph')}
+                        </span>
+                    </a>
+                </li>
+                <li className={Styles.line} onClick={onClick('line')}>
+                    <a href="#" onClick={handleAClick} role="button">
+                        {CommonUtils.getLang('DataAnalytics.line')}
+                        <span className={Styles.blind}>
+                            {CommonUtils.getLang('DataAnalytics.graph')}
+                        </span>
+                    </a>
+                </li>
+                <li className={Styles.pie} onClick={onClick('pie')}>
+                    <a href="#" onClick={handleAClick} role="button">
+                        {CommonUtils.getLang('DataAnalytics.pie')}
+                        <span className={Styles.blind}>
+                            {CommonUtils.getLang('DataAnalytics.graph')}
+                        </span>
+                    </a>
+                </li>
+                <li className={Styles.scatter} onClick={onClick('scatter')}>
+                    <a href="#" onClick={handleAClick} role="button">
+                        {CommonUtils.getLang('DataAnalytics.scatter')}
+                        <span className={Styles.blind}>
+                            {CommonUtils.getLang('DataAnalytics.graph')}
+                        </span>
+                    </a>
+                </li>
+            </ul>
+            <span className={`${Styles.arr}`}>
                 <i />
             </span>
         </div>
