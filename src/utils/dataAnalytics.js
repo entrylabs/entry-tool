3a74be626f1c08dd233ebc9c98ad3458a690b2b4
박영덕[SW교육플랫폼개발] <youngduk.park@nts-corp.com>
2020-12-21 18:06:44 +0900

    [dataAnalytics] 열의 데이터가 모두 숫자가 아닌 경우 - 으로 처리

--- src/utils/dataAnalytics.js
+++ src/utils/dataAnalytics.js
@@ -41,7 +41,7 @@ const makeSummary = (row) => {
     const restRow = row.slice(1);
     const count = restRow.length;
     if (someString(restRow)) {
-        return [row[0], count, '-', '-', '-', '-'];
+        return [row[0], '-', '-', '-', '-', '-'];
     }
     const max = Math.max(...restRow);
     const min = Math.min(...restRow);
