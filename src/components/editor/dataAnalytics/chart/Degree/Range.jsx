cba28f3bae1edfdbbe5466a13fec368df370120c
박영덕[SW교육플랫폼개발] <youngduk.park@nts-corp.com>
2021-02-05 20:06:17 +0900

    [Chart] 히스토그램차트 계급 폭 기능 추가

--- src/components/editor/dataAnalytics/chart/Degree/Range.jsx
+++ src/components/editor/dataAnalytics/chart/Degree/Range.jsx
@@ -1,8 +1,44 @@
-import React from 'react';
+import React, { useContext } from 'react';
+import { DataAnalyticsContext } from '@contexts/dataAnalytics';
+import _ceil from 'lodash/ceil';
+import _floor from 'lodash/floor';
+import _round from 'lodash/round';
+import _forEach from 'lodash/forEach';
 import Theme from '@utils/Theme';
 
 const Range = () => {
     const theme = Theme.getStyle('popup');
+    const { dataAnalytics } = useContext(DataAnalyticsContext);
+    const { selected = {} } = dataAnalytics;
+    const { table, chart = [], chartIndex = 0 } = selected;
+    const { categoryIndexes = [], bin = 5, boundary = 'left' } = chart[chartIndex];
+
+    const getValue = () => {
+        if (!categoryIndexes.length) {
+            return '';
+        }
+        let min = Number(table[1][categoryIndexes]) || 0;
+        let max = Number(table[1][categoryIndexes]) || 0;
+        _forEach(categoryIndexes, (index) => {
+            _forEach(table.slice(1), (row) => {
+                const value = Number(row[index]);
+                if (min > value) {
+                    min = value;
+                }
+                if (max < value) {
+                    max = value;
+                }
+            });
+        });
+        if (boundary === 'left' && _ceil(max) === max) {
+            max += 1;
+        }
+        if (boundary === 'right' && _floor(min) === min) {
+            min -= 1;
+        }
+        return _round((max - min - 1) / bin, 1);
+    };
+
     return (
         <div className={theme.select_group} style={{ marginLeft: 30 }}>
             <label htmlFor="CntWidth" className={theme.tit_label}>
@@ -16,7 +52,7 @@ const Range = () => {
                     className={theme.input}
                     id="Cnt"
                     name="Cnt"
-                    value="1.0"
+                    value={getValue()}
                     readOnly
                 />
             </div>
