4610241b48fd567c8674602cb6f4844b5218d1c1
Youngduk Park <0duck@kakao.com>
2022-12-09 13:33:41 +0900

    billboardjs 버전 업데이트, 그래프 모양 수정

--- src/components/editor/dataAnalytics/chart/ScatterGrid.jsx
+++ src/components/editor/dataAnalytics/chart/ScatterGrid.jsx
@@ -1,6 +1,6 @@
 import React, { useRef, useEffect, useState } from 'react';
 import { SCATTERGRID } from '@constants/dataAnalytics';
-import { corr, pcorr, pearsonCorrelation } from '@utils/dataAnalytics';
+import { corr } from '@utils/dataAnalytics';
 import bb from 'billboard.js';
 import '@assets/entry/scss/widget/insight.css';
 
@@ -9,37 +9,51 @@ const COEFFICIENT = 'coefficient';
 
 const getColumns = (table, indexes) => indexes.map((index) => table.map((field) => field[index]));
 
-const BillboardChart = ({ columns, type, xs, id = 'scatterGrid' }) => {
+const BillboardChart = ({ columns, type, xs, id = 'scatterGrid', xLabel, yLabel }) => {
     const chartRef = useRef(null);
     useEffect(() => {
-        bb.generate({ data: { columns, type, xs }, bindto: chartRef.current });
+        bb.generate({
+            data: { columns, type, xs },
+            bindto: chartRef.current,
+            point: { opacity: 1 },
+            axis: {
+                x: {
+                    label: xLabel,
+                },
+                y: {
+                    label: yLabel,
+                },
+            },
+            tooltip: { show: false },
+            legend: { show: false },
+        });
     }, []);
     return <div id={id} ref={chartRef} />;
 };
 
-const Chart = ({ table, categoryIndexes, mode }) => {
+const Chart = ({ table, categoryIndexes, mode, xAxis, yAxis }) => {
     const [x, y] = categoryIndexes;
-
     const columns = getColumns(table, categoryIndexes);
-    columns[1][0] += 'x';
-    if (mode !== ALL) {
+    const xLabel = xAxis ? columns[1][0] : '';
+    const yLabel = yAxis ? columns[0][0] : '';
+    if (mode === COEFFICIENT) {
         if (x === y) {
             return columns[0][0];
         } else if (x > y) {
-            console.log(
-                corr(columns[0].slice(1), columns[1].slice(1)),
-                pearsonCorrelation([columns[0].slice(1), columns[1].slice(1)])
-            );
-            return pcorr(columns[0].slice(1), columns[1].slice(1));
+            return corr(columns[0].slice(1), columns[1].slice(1));
         }
     }
-    console.log({ columns, type: 'scatter', categoryIndexes });
+    if (x == y) {
+        columns[1][0] += 'x';
+    }
     return (
         <BillboardChart
             id={SCATTERGRID + categoryIndexes[0] + categoryIndexes[1]}
             columns={columns}
             type={'scatter'}
             xs={{ [columns[0][0]]: columns[1][0] }}
+            xLabel={xLabel}
+            yLabel={yLabel}
         />
     );
 };
@@ -65,7 +79,7 @@ const ScatterGrid = ({ table, chart }) => {
                     borderCollapse: 'collapse',
                 }}
             >
-                {categoryIndexes.map((x) => (
+                {categoryIndexes.map((x, xIndex) => (
                     <tr
                         style={{
                             height: '30%',
@@ -73,7 +87,7 @@ const ScatterGrid = ({ table, chart }) => {
                             borderCollapse: 'collapse',
                         }}
                     >
-                        {categoryIndexes.map((y) => (
+                        {categoryIndexes.map((y, yIndex) => (
                             <td
                                 style={{
                                     width: '30%',
@@ -81,7 +95,13 @@ const ScatterGrid = ({ table, chart }) => {
                                     borderCollapse: 'collapse',
                                 }}
                             >
-                                <Chart table={table} categoryIndexes={[y, x]} mode={mode} />
+                                <Chart
+                                    table={table}
+                                    categoryIndexes={[x, y]}
+                                    mode={mode}
+                                    xAxis={xIndex === categoryIndexes.length - 1}
+                                    yAxis={yIndex === 0}
+                                />
                             </td>
                         ))}
                     </tr>
