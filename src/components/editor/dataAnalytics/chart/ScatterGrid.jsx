034c9a17a004e5fb752f374f52a5307a5250b0a3
Youngduk Park <0duck@kakao.com>
2022-12-14 18:53:44 +0900

    add scatter grid

--- src/components/editor/dataAnalytics/chart/ScatterGrid.jsx
+++ src/components/editor/dataAnalytics/chart/ScatterGrid.jsx
@@ -6,32 +6,84 @@ import '@assets/entry/scss/widget/insight.css';
 
 const ALL = 'all';
 const COEFFICIENT = 'coefficient';
+const size = 82;
+// const width = `${size}px`;
+// const height = `${size}px`;
 
 const getColumns = (table, indexes) => indexes.map((index) => table.map((field) => field[index]));
 
-const BillboardChart = ({ columns, type, xs, id = 'scatterGrid', xLabel, yLabel }) => {
+const BillboardChart = ({
+    columns,
+    type,
+    xs,
+    id = 'scatterGrid',
+    xLabel,
+    yLabel,
+    width,
+    height,
+}) => {
     const chartRef = useRef(null);
     useEffect(() => {
+        console.log({ width, height });
         bb.generate({
             data: { columns, type, xs },
             bindto: chartRef.current,
             point: { opacity: 1 },
             axis: {
                 x: {
-                    label: xLabel,
+                    tick: {
+                        count: 2,
+                        text: {
+                            show: xLabel,
+                        },
+                        culling: {
+                            lines: false,
+                        },
+                        format(x) {
+                            return Math.floor(x * 10) / 10;
+                        },
+                    },
+                    // show: false,
                 },
                 y: {
-                    label: yLabel,
+                    // show: false,
+                    tick: {
+                        // show: false,
+                        text: {
+                            show: false,
+                        },
+                        count: 2,
+                        culling: {
+                            lines: false,
+                            // max: 4,
+                        },
+                        format(x) {
+                            return Math.floor(x * 10) / 10;
+                        },
+                    },
+                    clipPath: false,
+                    // padding: -1,
                 },
             },
             tooltip: { show: false },
             legend: { show: false },
+            size: { width: width + 20, height: height + 20 },
         });
     }, []);
-    return <div id={id} ref={chartRef} />;
+    return (
+        <div
+            id={id}
+            ref={chartRef}
+            style={{
+                position: 'absolute',
+                marginLeft: '-20px',
+                // marginTop: '10px',
+            }}
+        />
+    );
 };
 
-const Chart = ({ table, categoryIndexes, mode, xAxis, yAxis }) => {
+const Chart = ({ table, categoryIndexes, mode, xAxis, yAxis, width, height }) => {
     const [x, y] = categoryIndexes;
     const columns = getColumns(table, categoryIndexes);
     const xLabel = xAxis ? columns[1][0] : '';
@@ -54,10 +106,11 @@ const Chart = ({ table, categoryIndexes, mode, xAxis, yAxis }) => {
             xs={{ [columns[0][0]]: columns[1][0] }}
             xLabel={xLabel}
             yLabel={yLabel}
+            width={width}
+            height={height}
         />
     );
 };
-
 const ScatterGrid = ({ table, chart }) => {
     const [mode, setMode] = useState(ALL);
     const { type, categoryIndexes } = chart;
@@ -73,37 +126,47 @@ const ScatterGrid = ({ table, chart }) => {
             <button onClick={handleClick}> hihi </button>
             <table
                 style={{
-                    width: '100%',
-                    height: '100%',
-                    border: '1px solid black',
+                    // width: '100%',
+                    // height: '100%',
+                    border: '1px solid white',
                     borderCollapse: 'collapse',
                 }}
             >
                 {categoryIndexes.map((x, xIndex) => (
                     <tr
                         style={{
-                            height: '30%',
-                            border: '1px solid black',
+                            // height,
+                            border: '1px solid white',
                             borderCollapse: 'collapse',
                         }}
                     >
-                        {categoryIndexes.map((y, yIndex) => (
-                            <td
-                                style={{
-                                    width: '30%',
-                                    border: '1px solid black',
-                                    borderCollapse: 'collapse',
-                                }}
-                            >
-                                <Chart
-                                    table={table}
-                                    categoryIndexes={[x, y]}
-                                    mode={mode}
-                                    xAxis={xIndex === categoryIndexes.length - 1}
-                                    yAxis={yIndex === 0}
-                                />
-                            </td>
-                        ))}
+                        {categoryIndexes.map((y, yIndex) => {
+                            const xAxis = xIndex === categoryIndexes.length - 1;
+                            const yAxis = yIndex === 0;
+                            const width = size;
+                            const height = size;
+
+                            return (
+                                <td
+                                    style={{
+                                        width: `${width + 20}px`,
+                                        height: `${height + 20}px`,
+                                        border: '1px solid white',
+                                        borderCollapse: 'collapse',
+                                    }}
+                                >
+                                    <Chart
+                                        table={table}
+                                        categoryIndexes={[x, y]}
+                                        mode={mode}
+                                        xAxis={xAxis}
+                                        yAxis={yAxis}
+                                        width={width}
+                                        height={height}
+                                    />
+                                </td>
+                            );
+                        })}
                     </tr>
                 ))}
             </table>
