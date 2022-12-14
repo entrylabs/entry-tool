c4e8d83215dcb8f8aef1607f23236186d16f8abb
Youngduk Park <0duck@kakao.com>
2022-12-15 00:40:55 +0900

    요약 탭 차트 레이아웃 수정

--- src/components/editor/dataAnalytics/summary/ChartList.jsx
+++ src/components/editor/dataAnalytics/summary/ChartList.jsx
@@ -1,17 +1,15 @@
 import React, { useContext } from 'react';
-import Chart from '@components/widget/Chart';
 import { DataAnalyticsContext } from '@contexts/dataAnalytics';
 import { CHART } from '@constants/dataAnalytics';
-import { getTrimedTable } from '@utils/dataAnalytics';
 import Theme from '@utils/Theme';
 import { CommonUtils } from '@utils/Common';
+import cx from 'classnames';
 
 const ChartList = () => {
     const theme = Theme.getStyle('popup');
     const { dataAnalytics, dispatch } = useContext(DataAnalyticsContext);
     const { selected } = dataAnalytics;
-    const { table: selectedTable, chart: charts = [] } = selected;
-    const table = getTrimedTable(selectedTable);
+    const { chart: charts = [] } = selected;
 
     const handleClickChart = (tab, index) => () => {
         dispatch({
@@ -21,64 +19,32 @@ const ChartList = () => {
         });
     };
 
-    const handleMouseEnter = (event) => {
-        event.currentTarget.classList.add(theme.on);
-    };
-
-    const handleMouseLeave = (event) => {
-        event.currentTarget.classList.remove(theme.on);
-    };
-
     return (
         <div className={theme.category_box}>
+            <div className={theme.table_sjt}>
+                <strong>{CommonUtils.getLang('DataAnalytics.chart')}</strong>
+                {!charts.length ? (
+                    <p className={theme.title_dsc}>
+                        {CommonUtils.getLang('DataAnalytics.not_exist_chart')}
+                    </p>
+                ) : null}
+            </div>
             {charts.length ? (
                 <div className={theme.chart_list}>
-                    <ul className={theme.list}>
+                    <ul className={theme.chart_list}>
                         {charts.map((chart, index) => (
-                            <li
-                                key={`summary_chart_${index}`}
-                                className={theme[chart.type]}
-                                onMouseEnter={handleMouseEnter}
-                                onMouseLeave={handleMouseLeave}
-                                onClick={handleClickChart(CHART, index)}
-                            >
-                                <Chart
-                                    chart={chart}
-                                    table={table}
-                                    size={{
-                                        width: 190,
-                                        height: 128,
-                                    }}
-                                    legend={{ show: false }}
-                                    tooltip={{ show: false }}
-                                    axisX={{
-                                        tick: {
-                                            show: false,
-                                            text: {
-                                                show: false,
-                                            },
-                                        },
-                                    }}
-                                    axisY={{
-                                        tick: {
-                                            culling: true,
-                                        },
-                                    }}
-                                    shortForm={true}
-                                    key={`chart_${index}`}
-                                />
+                            <li key={`summary_chart_${index}`}>
+                                <button
+                                    className={cx(theme.chart_link, theme[chart.type])}
+                                    onClick={handleClickChart(CHART, index)}
+                                >
+                                    <strong className={theme.title}>{chart.title}</strong>
+                                </button>
                             </li>
                         ))}
                     </ul>
                 </div>
-            ) : (
-                <div className={theme.table_sjt}>
-                    <strong>{CommonUtils.getLang('DataAnalytics.chart')}</strong>
-                    <p className={theme.title_dsc}>
-                        {CommonUtils.getLang('DataAnalytics.not_exist_chart')}
-                    </p>
-                </div>
-            )}
+            ) : null}
         </div>
     );
 };
