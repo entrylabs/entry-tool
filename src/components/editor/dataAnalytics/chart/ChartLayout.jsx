4179e4da823231fd9421ebfbe3b0089e075baac2
joohee.moon <joohee.moon@nts-corp.com>
2022-12-27 11:39:04 +0900

    [#390] [WS] 데이터 분석에 새로운 기능(산점도 행렬, 차트 크게 보기 등)을 추가 - 디자인 검수 반영
    - https://oss.navercorp.com/entry/entry2/issues/5209#issuecomment-9287420

--- src/components/editor/dataAnalytics/chart/ChartLayout.jsx
+++ src/components/editor/dataAnalytics/chart/ChartLayout.jsx
@@ -115,7 +115,7 @@ const ChartLayout = () => {
                         {CommonUtils.getLang('DataAnalytics.remove_chart')}
                     </a>
                 </div>
-                <div className={`${theme.input_inner} ${theme.margin60}`}>
+                <div className={theme.input_inner}>
                     {xAxis ? <XAxis key={`${key}_xaxis`} /> : ''}
                     {yAxis ? <YAxis key={`${key}_yaxis`} /> : ''}
                     {category ? (
