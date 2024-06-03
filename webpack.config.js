43b454e16666787842d13316ea72fec516d2ac4a
usbsync <usbsync@nts-corp.com>
2024-06-03 12:09:09 +0900

    webpack4 -> webpack5

--- webpack.config.js
+++ webpack.config.js
@@ -1,26 +1,15 @@
 const path = require('path');
 const webpack = require('webpack');
-const pkg = require('./package.json');
-const SafeUmdPlugin = require('safe-umd-webpack-plugin');
-const FixStyleOnlyEntriesPlugin = require('webpack-fix-style-only-entries');
 const MiniCssExtractPlugin = require('mini-css-extract-plugin');
 const autoprefixer = require('autoprefixer');
-const options = {};
-if (process.env.NODE_ENV === 'watch') {
-    options.watch = true;
-}
-
-const crypto = require('crypto');
 
 const BANNER = [
     'ENTRY TOOL by @entrylabs',
-    `@version ${pkg.version} | ${new Date().toDateString()}`,
-    `@author ${pkg.author}`,
-    `@license ${pkg.license}`,
+    `@version ${require('./package.json').version} | ${new Date().toDateString()}`,
+    `@license ${require('./package.json').license}`,
 ].join('\n');
 
-const config = {
-    ...options,
+module.exports = {
     entry: {
         'entry-tool': './src/index.jsx',
     },
@@ -29,6 +18,7 @@ const config = {
         filename: '[name].js',
         library: 'EntryTool',
         libraryTarget: 'umd',
+        clean: true,
     },
     module: {
         rules: [
@@ -39,66 +29,73 @@ const config = {
             },
             {
                 test: /\.(css|scss)$/,
-                include: [
-                    path.resolve('./node_modules/'),
-                    path.resolve('./src/assets/entry/scss/widget/insight.css'),
-                ],
-                use: [require.resolve('style-loader'), require.resolve('css-loader')],
-            },
-            {
-                test: /\.(css|s[ac]ss)$/i,
-                exclude: [
-                    path.resolve('./node_modules/'),
-                    path.resolve('./src/assets/entry/scss/widget/insight.css'),
-                ],
-                use: [
-                    MiniCssExtractPlugin.loader,
+                oneOf: [
                     {
-                        loader: 'css-loader',
-                        options: {
-                            modules: true,
-                            sourceMap: true,
-                            importLoaders: 2,
-                            getLocalIdent: (context, localIdentName, localName, options) => {
-                                const hash = crypto
-                                    .createHash('sha256')
-                                    .update(context.resourcePath)
-                                    .digest('hex')
-                                    .substr(0, 5);
-                                return `${localName}__${hash}`;
+                        test: /\.(css|scss)$/,
+                        include: [
+                            path.resolve('./node_modules/'),
+                            path.resolve('./src/assets/entry/scss/widget/insight.css'),
+                        ],
+                        use: [
+                            require.resolve('style-loader'),
+                            require.resolve('css-loader'),
+                            require.resolve('sass-loader'),
+                        ],
+                    },
+                    {
+                        test: /\.(css|scss)$/,
+                        exclude: [
+                            path.resolve('./node_modules/'),
+                            path.resolve('./src/assets/entry/scss/widget/insight.css'),
+                        ],
+                        use: [
+                            MiniCssExtractPlugin.loader,
+                            {
+                                loader: 'css-loader',
+                                options: {
+                                    modules: {
+                                        localIdentName: '[local]__[hash:base64:5]',
+                                        exportLocalsConvention: 'camelCase',
+                                        exportOnlyLocals: false, // This should be false to export both classnames and locals
+                                    },
+                                    sourceMap: true,
+                                    importLoaders: 2,
+                                },
                             },
-                        },
-                    },
-                    {
-                        loader: 'postcss-loader',
-                        options: {
-                            sourceMap: true,
-                            plugins: [
-                                require('postcss-flexbugs-fixes'),
-                                autoprefixer({
-                                    browsers: [
-                                        '>1%',
-                                        'last 4 versions',
-                                        'Firefox ESR',
-                                        'not ie < 9', // React doesn't support IE8 anyway
-                                    ],
-                                    flexbox: 'no-2009',
-                                    remove: false,
-                                }),
-                            ],
-                        },
-                    },
-                    {
-                        loader: 'sass-loader',
-                        options: {
-                            sourceMap: true,
-                        },
+                            {
+                                loader: 'postcss-loader',
+                                options: {
+                                    sourceMap: true,
+                                    postcssOptions: {
+                                        plugins: [
+                                            require('postcss-flexbugs-fixes'),
+                                            autoprefixer({
+                                                overrideBrowserslist: [
+                                                    '>1%',
+                                                    'last 4 versions',
+                                                    'Firefox ESR',
+                                                    'not ie < 9',
+                                                ],
+                                                flexbox: 'no-2009',
+                                                remove: false,
+                                            }),
+                                        ],
+                                    },
+                                },
+                            },
+                            {
+                                loader: 'sass-loader',
+                                options: {
+                                    sourceMap: true,
+                                },
+                            },
+                        ],
                     },
                 ],
             },
             {
-                test: [/\.bmp$/, /\.gif$/, /\.jpe?g$/, /\.png$/, /\.svg$/],
-                use: 'url-loader',
+                test: /\.(png|jpe?g|gif|svg)$/i,
+                type: 'asset/inline',
             },
         ],
     },
@@ -123,8 +120,6 @@ const config = {
         },
     },
     plugins: [
-        new SafeUmdPlugin(),
-        new FixStyleOnlyEntriesPlugin(),
         new webpack.BannerPlugin({
             banner: BANNER,
             entryOnly: true,
@@ -134,8 +129,9 @@ const config = {
         }),
     ],
     optimization: {
-        sideEffects: false,
+        sideEffects: true,
+        usedExports: true,
+        minimize: true,
     },
+    mode: process.env.NODE_ENV || 'development',
 };
-
-module.exports = config;
