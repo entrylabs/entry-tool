ddd13f2e20356845ace60464bb0067057c8e7899
JY Kim <kimorkim@gmail.com>
2024-07-23 11:48:22 +0900

    Update build.sh

--- scripts/build.sh
+++ scripts/build.sh
@@ -4,7 +4,7 @@ set -e # 에러 발생 시 스크립트 중단
 export NODE_ENV=production
 git clone -b build "${GH_REPO}" build
 rm -rf build/**/* || exit 0
-yarn dist
+pnpm dist
 cp -r dist build/
 ls -al
 ls -al build
